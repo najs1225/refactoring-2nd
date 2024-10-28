b1a85d74c4256d14dcfaa8fed608a5ca0eb9f57b
najs1225 <najs1225@gmail.com>
2024-10-28 20:33:22 +0900

    241028 리팩토링 chap01 정리

--- chap01/chap01_ex01.js
+++ chap01/chap01_ex01.js
@@ -1,44 +1,40 @@
-public String statement(Invoice invoice, Plays plays) throws Exception {
-    int totalAmount = 0;
-    int volumeCredit = 0;
-    StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
-    for (Performance performance : invoice.getPerformances()) {
-        Play play = plays.get(performance);
-        int thisAmount = 0;
-    
-        // 여기에 주목 
-        switch (play.getType()) {
-            case TRAGEDY:
-                thisAmount = 40000;
-                if (performance.getAudience() > 30) {
-                    thisAmount += 1000 * (performance.getAudience() - 30);
-                }
-                break;
-            case COMEDY:
-                thisAmount = 30000;
-                if (performance.getAudience() > 20) {
-                    thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-                }
-                thisAmount += 300 * performance.getAudience();
-                break;
-            default:
-                throw new Exception("알 수 없는 장르");
-        }
-
-        // 포인트를 적립한다.
-        volumeCredit += Math.max(performance.getAudience() - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공핟나.
-        if (play.getType().equals(PlayType.COMEDY)) {
-            volumeCredit += Math.floor(performance.getAudience() / 5);
-        }
-
-        // 청구 내역을 출력한다.
-        result.append(String.format("%s: $%d %d석\n",play.getName(), thisAmount / 100, performance.getAudience()));
-        totalAmount += thisAmount;
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+    const format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 });
+  
+    for (const perf of invoice.performances) {
+      const play = plays[perf.playID];
+      let thisAmount = 0;
+  
+      switch (play.type) {
+        case 'tragedy':
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case 'comedy':
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${play.type}`);
+      }
+  
+      volumeCredits += Math.max(perf.audience - 30, 0);
+  
+      if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);
+  
+      result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+      totalAmount += thisAmount;
     }
-
-    result.append(String.format("총액: $%d\n",totalAmount / 100));
-    result.append(String.format("적립 포인트: %d점", volumeCredit));
-    return result.toString();
-}
+  
+    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n;`;
+    return result;
+  }
